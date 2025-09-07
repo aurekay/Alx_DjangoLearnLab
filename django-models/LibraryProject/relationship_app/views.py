acfe900f7ef8dd52b7c037dadb29d430c71e5c8a
aureliusk <eddykay2010@gmail.com>
2025-09-07 23:17:53 +0000

    Implement LibraryDetailView with Django DetailView and update urls

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Book, Library
 
 # Function-based view: render all books with template
@@ -7,9 +7,15 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view: use DetailView to show a library and its books
+# Class-based view: use DetailView for a library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+    # Ensure books for the library are available in context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = self.object.books.all()
+        return context
 
