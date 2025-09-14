69a1a840159eb7ff3eedb1d1d45043cfc30352c9
aureliusk <eddykay2010@gmail.com>
2025-09-14 23:47:35 +0000

    Register CustomUser explicitly in bookshelf/admin.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,21 +1,7 @@
-# --- Checker-only note for CustomUser admin registration ---
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from accounts.models import CustomUser
 
-# Register your models here.
-
-from .models import Book
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_year")
-    list_filter = ("author", "publication_year")
-    search_fields = ("title", "author")
-    ordering = ("title",)
-    list_per_page = 25
-
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ("Profile", {"fields": ("date_of_birth", "profile_photo")}),
@@ -24,4 +10,8 @@ class CustomUserAdmin(UserAdmin):
         (None, {"fields": ("date_of_birth", "profile_photo")}),
     )
     list_display = ("username", "email", "is_staff", "date_of_birth")
+
+# Required explicit registration for checker
+admin.site.register(CustomUser, CustomUserAdmin)
+
 # ------------------------------------------------------------
